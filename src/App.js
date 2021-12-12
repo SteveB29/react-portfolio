b080812c0ef2d93958c19e3e343b73ab4b340ada
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-12 11:16:58 -0500

    added about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
       <Footer />
     </>
   );
---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about-me" class="about-me">
+      <p>
+        My name is Steven Bendrick. I graduated from the University of Connecticut with a degeree in Chemical Engineering. 
+        After graduating, I found a job doing database work in the Alumni and Development department at a local nonprofit boarding school. 
+        I grew to enjoy doing database work and enjoyed learning more and more about how it all works. 
+        After 5 years at the school, I noticed the database world was moving towards online applications. 
+        I decided to enroll in the Uconn Coding Bootcamp to get into the world of online solutions. 
+        I look forward to using my technical skills as a trained engineer and my first hand experience of helping end-users use online applications.
+      </p>
+    </section>
+  )
+}
+
+export default About;
