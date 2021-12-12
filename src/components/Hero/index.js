c6fb5def3de3e028f64e4d5d96d4139b124b1a99
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-12 11:05:34 -0500

    added hero

---
+++ src/components/Hero/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+
+  return (
+    <div className="hero" style={{ backgroundImage: 'url(/images/hero-background.jpg)' }}>
+        <p>
+            Details Matter
+        </p>
+    </div>
+  )
+}
+
+export default Hero;
