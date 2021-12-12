ec7da7ddd193c64bf889a3f97d6351fbf5d831b6
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-12 10:30:43 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <header>
-        <h1>Steve Bendrick</h1>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#about-me">About Me</a>
-                </li>
-                <li>
-                    <a href="#work">Work</a>
-                </li>
-                <li>
-                    <a href="#contact-me">Contact Me</a>
-                </li>
-                <li>
-                    <a href="./assets/Resume/Steven-Bendrick-Resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <Header />
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+        <h1>Steve Bendrick</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#about-me">About Me</a>
+                </li>
+                <li>
+                    <a href="#work">Work</a>
+                </li>
+                <li>
+                    <a href="#contact-me">Contact Me</a>
+                </li>
+                <li>
+                    <a href="./assets/Resume/Steven-Bendrick-Resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
