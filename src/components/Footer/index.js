c40a212c63941b8f27765a087ac6c8a4346b28c4
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-12 11:13:50 -0500

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer id="contact-me">
+      <address>
+          <ul>
+              <li>
+                  <a href="mailto:Steven.Bendrick@gmail.com" target="_blank" rel="noreferrer noopener">Steven.Bendrick@gmail.com</a>
+              </li>
+              <li>
+                  <a href="tel:+12037889035">203-788-9035</a>
+              </li>
+              <li>
+                  <a href="https://github.com/SteveB29" target="_blank" rel="noreferrer noopener">GitHub</a>
+              </li>
+              <li>
+                  <a href="https://www.linkedin.com/in/steven-bendrick-7778b5134/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+              </li>
+          </ul>
+      </address>
+    </footer>
+  )
+}
+
+export default Footer;
