b1cc94a7f52f9a3c058cd88733b964ff3ed4e687
Steve Bendrick <steven.bendrick@gmail.com>
2021-12-12 10:35:02 -0500

    split navigation from Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,11 @@
 import React from "react";
+import Navigation from "../Navigation";
 
 function Header() {
   return (
     <header>
         <h1>Steve Bendrick</h1>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#about-me">About Me</a>
-                </li>
-                <li>
-                    <a href="#work">Work</a>
-                </li>
-                <li>
-                    <a href="#contact-me">Contact Me</a>
-                </li>
-                <li>
-                    <a href="./assets/Resume/Steven-Bendrick-Resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
-                </li>
-            </ul>
-        </nav>
+        <Navigation />
     </header>
   )
 }
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+            <a href="#about-me">About Me</a>
+        </li>
+        <li>
+            <a href="#work">Work</a>
+        </li>
+        <li>
+            <a href="#contact-me">Contact Me</a>
+        </li>
+        <li>
+            <a href="./assets/Resume/Steven-Bendrick-Resume.pdf" target="_blank" rel="noreferrer noopener">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
